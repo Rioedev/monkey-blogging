06f9467c94cdaa6d020cfbaa68e033cb336d5008
Rioedev <lequy11082003@gmail.com>
2023-06-11 13:12:25 +0700

    ui sign-up and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { Routes } from "react-router-dom";
 import { AuthProvider } from "./contexts/auth-context";
+import { Route } from "react-router-dom";
+import "./styles/index.scss";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
     <div>
       <AuthProvider>
-        <Routes></Routes>
+        <Routes>
+          <Route path="/sign-up" element={<SignUpPage></SignUpPage>}></Route>
+        </Routes>
       </AuthProvider>
     </div>
   );
