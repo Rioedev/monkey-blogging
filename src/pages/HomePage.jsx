3a91ce2e78860e35c2cdf74233fd799d5d2467ab
Rioedev <lequy11082003@gmail.com>
2023-06-14 23:14:57 +0700

    heading & home UI

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,9 @@
-import { signOut } from "firebase/auth";
 import React from "react";
-import { auth } from "../firebases/firebase-config";
 import styled from "styled-components";
-import Header from "../components/layout/Header";
 import HomeBanner from "../module/home/HomeBanner";
 import Layout from "../components/layout/Layout";
+import HomeFeature from "../module/home/HomeFeature";
+import HomeNewest from "../module/home/HomeNewest";
 
 const HomePageStyles = styled.div``;
 
@@ -13,6 +12,8 @@ const HomePage = () => {
     <HomePageStyles>
       <Layout>
         <HomeBanner></HomeBanner>
+        <HomeFeature></HomeFeature>
+        <HomeNewest></HomeNewest>
       </Layout>
     </HomePageStyles>
   );
