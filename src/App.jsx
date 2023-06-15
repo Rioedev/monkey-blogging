8120031b6e9b6ccb02977559373e0687aeb83000
Rioedev <lequy11082003@gmail.com>
2023-06-15 16:24:41 +0700

    PostDetails Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Routes } from "react-router-dom";
 import { AuthProvider } from "./contexts/auth-context";
 import { Route } from "react-router-dom";
-import "./styles/index.scss";
 import SignUpPage from "./pages/SignUpPage";
 import SignInPage from "./pages/SignInPage";
 import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
+import PostDetailsPage from "./pages/PostDetailsPage";
 
 function App() {
   return (
@@ -16,6 +16,10 @@ function App() {
           <Route path="/sign-up" element={<SignUpPage></SignUpPage>}></Route>
           <Route path="/sign-in" element={<SignInPage></SignInPage>}></Route>
           <Route path="*" element={<NotFoundPage></NotFoundPage>}></Route>
+          <Route
+            path="/:slug"
+            element={<PostDetailsPage></PostDetailsPage>}
+          ></Route>
         </Routes>
       </AuthProvider>
     </div>
