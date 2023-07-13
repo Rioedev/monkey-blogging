8dfe67871e21e7f68293e8e2dd6ef172b44434c9
Rioedev <lequy11082003@gmail.com>
2023-07-13 11:24:41 +0700

    update post

---
+++ src/api/apiConfig.js
@@ -0,0 +1,1 @@
+export const imgbbAPI = `https://api.imgbb.com/1/upload?key=16d4fe9fb6724c5fe700df7f7e06d5ca`;
--- src/module/post/PostUpdate.jsx
+++ src/module/post/PostUpdate.jsx
@@ -1,13 +1,270 @@
-import React from "react";
+import { useState } from "react";
 import DashboardHeading from "../dashboard/DashboardHeading";
+import Button from "../../components/button/Button";
+import Radio from "../../components/checkbox/Radio";
+import { postStatus } from "../../utils/constants";
+import { Label } from "../../components/label";
+import { Field } from "../../components/field";
+import Toggle from "../../components/toggle/Toggle";
+import { Dropdown } from "../../components/dropdown";
+import ImageUpload from "../../components/image/ImageUpload";
+import { Input } from "../../components/input";
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import { db } from "../../firebases/firebase-config";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import useFirebaseImage from "../../hooks/useFirebaseImage";
+import ReactQuill, { Quill } from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { toast } from "react-toastify";
+import ImageUploader from "quill-image-uploader";
+import { useMemo } from "react";
+import { imgbbAPI } from "../../api/apiConfig";
+import axios from "axios";
+
+Quill.register("modules/imageUploader", ImageUploader);
 
 const PostUpdate = () => {
+  const [params] = useSearchParams();
+  const postId = params.get("id");
+  const [content, setContent] = useState("");
+  const {
+    handleSubmit,
+    control,
+    setValue,
+    watch,
+    reset,
+    getValues,
+    formState: { isValid, isSubmitting },
+  } = useForm({
+    mode: "onChange",
+  });
+  const imageUrl = getValues("image");
+  const imageName = getValues("image_name");
+  const { image, setImage, progress, handleSelectImage, handleDeleteImage } =
+    useFirebaseImage(setValue, getValues, imageName, deletePostImage);
+  async function deletePostImage() {
+    const colRef = doc(db, "users", postId);
+    await updateDoc(colRef, {
+      avatar: "",
+    });
+  }
+  useEffect(() => {
+    setImage(imageUrl);
+  }, [imageUrl, setImage]);
+
+  const watchHot = watch("hot");
+  const watchStatus = watch("status");
+  useEffect(() => {
+    async function fetchData() {
+      if (!postId) return;
+      const docRef = doc(db, "posts", postId);
+      const docSnapshot = await getDoc(docRef);
+      if (docSnapshot.data()) {
+        reset(docSnapshot.data());
+        setSelectCategory(docSnapshot.data()?.category || "");
+        setContent(docSnapshot.data()?.content || "");
+      }
+    }
+    fetchData();
+  }, [postId, reset]);
+  const [categories, setCategories] = useState([]);
+  const [selectCategory, setSelectCategory] = useState("");
+  useEffect(() => {
+    async function getCategoriesData() {
+      const colRef = collection(db, "categories");
+      const q = query(colRef, where("status", "==", 1));
+      const querySnapshot = await getDocs(q);
+      let result = [];
+      querySnapshot.forEach((doc) => {
+        result.push({
+          id: doc.id,
+          ...doc.data(),
+        });
+      });
+      setCategories(result);
+    }
+    getCategoriesData();
+  }, []);
+  const handleClickOption = async (item) => {
+    const colRef = doc(db, "categories", item.id);
+    const docData = await getDoc(colRef);
+    setValue("category", {
+      id: docData.id,
+      ...docData.data(),
+    });
+    // setValue("categoryId", item.id);
+    setSelectCategory(item);
+  };
+
+  const updatePostHandler = async (values) => {
+    if (!isValid) return;
+    const docRef = doc(db, "posts", postId);
+    await updateDoc(docRef, {
+      ...values,
+      content,
+    });
+    toast.success(`Updated post successfully`);
+  };
+  const modules = useMemo(
+    () => ({
+      toolbar: [
+        ["bold", "italic", "underline", "strike"],
+        ["blockquote"],
+        [{ header: 1 }, { header: 2 }],
+        [{ list: "ordered" }, { list: "bullet" }],
+        [{ header: [1, 2, 3, 4, 5, 6, false] }],
+        ["link", "image"],
+      ],
+      imageUploader: {
+        upload: async (file) => {
+          const bodyFormData = new FormData();
+          bodyFormData.append("image", file);
+          const reponse = await axios({
+            method: "POST",
+            url: imgbbAPI,
+            data: bodyFormData,
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          });
+          return reponse.data.data.url;
+        },
+      },
+    }),
+    []
+  );
+  if (!postId) return null;
   return (
     <>
       <DashboardHeading
         title="Update post"
         desc="Update post content"
       ></DashboardHeading>
+      <form onSubmit={handleSubmit(updatePostHandler)}>
+        <div className="form-layout">
+          <Field>
+            <Label>Title</Label>
+            <Input
+              control={control}
+              placeholder="Enter your title"
+              name="title"
+            ></Input>
+          </Field>
+          <Field>
+            <Label>Slug</Label>
+            <Input
+              control={control}
+              placeholder="Enter your slug"
+              name="slug"
+            ></Input>
+          </Field>
+        </div>
+        <div className="form-layout">
+          <Field>
+            <Label>Image</Label>
+            <ImageUpload
+              onChange={handleSelectImage}
+              handleDeleteImage={handleDeleteImage}
+              className="h-[250px]"
+              progress={progress}
+              image={image}
+            ></ImageUpload>
+          </Field>
+          <Field>
+            <Label>Category</Label>
+            <Dropdown>
+              <Dropdown.Select
+                placeholder={`${selectCategory.name || "Select the category"}`}
+              ></Dropdown.Select>
+              <Dropdown.List>
+                {categories.length > 0 &&
+                  categories.map((item) => (
+                    <Dropdown.Option
+                      key={item.id}
+                      onClick={() => handleClickOption(item)}
+                    >
+                      {item.name}
+                    </Dropdown.Option>
+                  ))}
+              </Dropdown.List>
+            </Dropdown>
+            {selectCategory?.name && (
+              <span className="inline-block text-green-600 p-3 rounded-lg text-sm font-medium bg-green-100">
+                {selectCategory?.name}
+              </span>
+            )}
+          </Field>
+        </div>
+        <div className="mb-10">
+          <Field>
+            <Label>Content</Label>
+            <div className="w-full entry-content">
+              <ReactQuill
+                modules={modules}
+                theme="snow"
+                value={content}
+                onChange={setContent}
+              />
+            </div>
+          </Field>
+        </div>
+        <div className="form-layout">
+          <Field>
+            <Label>Feature post</Label>
+            <Toggle
+              on={watchHot === true}
+              onClick={() => setValue("hot", !watchHot)}
+            ></Toggle>
+          </Field>
+          <Field>
+            <Label>Status</Label>
+            <div className="flex items-center gap-x-5">
+              <Radio
+                name="status"
+                control={control}
+                checked={Number(watchStatus) === postStatus.APPROVED}
+                value={postStatus.APPROVED}
+              >
+                Approved
+              </Radio>
+              <Radio
+                name="status"
+                control={control}
+                checked={Number(watchStatus) === postStatus.PENDING}
+                value={postStatus.PENDING}
+              >
+                Pending
+              </Radio>
+              <Radio
+                name="status"
+                control={control}
+                checked={Number(watchStatus) === postStatus.REJECTED}
+                value={postStatus.REJECTED}
+              >
+                Reject
+              </Radio>
+            </div>
+          </Field>
+        </div>
+        <Button
+          type="submit"
+          className="mx-auto w-full"
+          isLoading={isSubmitting}
+          disabled={isSubmitting}
+        >
+          Update post
+        </Button>
+      </form>
     </>
   );
 };
